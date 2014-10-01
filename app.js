3183744964524a46cdfb039703334c784bb07f0c
Bobby Earl <bobby@simplyearl.com>
2014-10-01 19:13:13 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,72 @@
+!function($) {
+  $(function() {
+    
+    var route = [
+      { index: 307 }, 
+      { index: 308 }, 
+      { index: 309 },
+      { index: 313 },
+      { index: 319 },
+      { index: 310 },
+      { index: 311 },
+      { index: 312 },
+      // { index: 314 },
+      { index: 293 },
+      { index: 294 },
+      { index: 295 },
+      { index: 296 },
+      { index: 297 },
+      { index: 298 },
+      { index: 315 },
+      { index: 316 }
+    ];
+    
+    $.getJSON('cameras-2014-10-01.json', function(data) {
+      
+      Handlebars.registerHelper('if_eq', function(a, b, opts) {
+        return a == b ? opts.fn(this) : opts.inverse(this);
+      });
+      
+      // Reverse route in afternoon
+      if ( (new Date()).getHours() > 12) {
+        route.reverse();
+      }
+      
+      // Find route
+      for ( var i = 0, j = route.length; i < j; i++) {
+        route[i].feature = data.features[route[i].index];
+      }
+      
+      // Build route
+      $('.compiled-routes').html(Handlebars.compile($('#template-route').html())(route));
+      
+      // Initialize route video
+      for (var i = 0, j = route.length; i < j; i++) {
+        jwplayer('route-' + i).setup({
+          autostart: true,
+          stretching: 'fill',
+          width: '100%',
+          aspectratio: '4:3',
+          file: route[i].feature.properties.rtmp_url
+        })
+      }
+      
+      // Build camera list
+      $('.compiled-cameras').html(Handlebars.compile($('#template-cameras').html())(data));      
+      
+      // Load requested camera
+      $('.camera').click(function(e) {
+        e.stopPropagation();
+        jwplayer('player').setup({
+          autostart: true,
+          stretching: 'fill',
+          width: '100%',
+          aspectratio: '4:3',
+          file: data.features[$(this).data('index')].properties.rtmp_url
+        });
+      });
+      
+    });
+    
+  });
+}(jQuery);
