1b9046eb26f7ca31d114382dc838e9f9d3d6d0bd
Bobby Earl <bobby.earl@blackbaud.com>
2016-12-19 12:45:21 -0500

    Updates

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,168 +4,100 @@
   var camera = [];
   var route = [
     {
-      name: 'I-526 (West)',
+      name: '526 E',
       camera: [
-        { id: 60039 },
-        { id: 60040 },
-        { id: 60045 },
-        { id: 60042 },
-        { id: 60022 },
-        { id: 60023 },
-        { id: 60043 }
-      ]
-    },
-    {
-      name: 'I-526 (East)',
-      camera: [
-        // { id: 60044 },
         { id: 60025 },
         { id: 60026 },
         { id: 60027 },
         { id: 60028 },
         { id: 60029 },
         { id: 60030 },
+        { id: 60043 },
+        // { id: 60044 }, Not working as of 2016-12-19
         { id: 60046 },
         { id: 60047 },
-        // { id: 60048 },
-        // { id: 60049 },
-        // { id: 60050 }
+        { id: 60048 },
+        { id: 60049 }
+        // { id: 60050 } Not working as of 2016-12-19
       ]
     },
     {
-      name: 'I-26 (220 - 213)',
+      name: '526 W',
       camera: [
-        { id: 60063 },
-        { id: 60062 },
-        { id: 60061 },
-        { id: 60060 },
-        { id: 60059 },
-        { id: 60074 },
-        { id: 60035 },
-        { id: 60034 },
-        { id: 60033 },
-        { id: 60032 },
-        { id: 60031 },
-        { id: 60038 }
+        { id: 60039 },
+        { id: 60040 },
+        { id: 60041 },
+        { id: 60042 },
+        { id: 60045 },
+        { id: 60052 },
+        { id: 60054 }
       ]
     },
     {
-      name: 'I-26 (212 - 210)',
+      name: '26 Outer',
       camera: [
-        { id: 60023 },
-        { id: 60037 },
-        { id: 60036 },
-        { id: 60024 },
-        { id: 60021 },
-        { id: 60020 },
-        { id: 60019 },
-        { id: 60018 },
-        { id: 60017 },
-        { id: 60016 },
-        { id: 60015 },
-        { id: 60014 },
-      ]
-    },
-    {
-      name: 'I-26 (210 - 199)',
-      camera: [
-        { id: 60013 },
-        { id: 60095 },
-        { id: 60012 },
-        { id: 60010 },
-        { id: 60009 },
-        { id: 60008 },
-        { id: 60007 },
-        { id: 60006 },
+        { id: 60002 },
+        { id: 60003 },
+        { id: 60004 },
         { id: 60005 },
+        { id: 60006 },
+        { id: 60007 },
+        { id: 60008 },
         { id: 60096 },
-        { id: 60002 }
+        { id: 60097 }
       ]
     },
     {
-      name: 'I-26 at I-95',
+      name: '26 Middle',
       camera: [
-        { id: 10101 },
-        { id: 10102 },
-        { id: 10104 },
-        { id: 10105 },
-        { id: 10106 },
-        { id: 10108 },
-        { id: 10100 },
-        { id: 10099 },
-        { id: 10098 },
-        { id: 10097 },
-        { id: 10096 }
+        { id: 60009 },
+        { id: 60010 },
+        { id: 60012 },
+        // { id: 60013 }, Not working as of 2016-12-19
+        { id: 60014 },
+        { id: 60015 },
+        { id: 60016 },
+        { id: 60017 },
+        { id: 60018 },
+        { id: 60019 },
+        { id: 60020 },
+        { id: 60021 },
+        { id: 60022 },
+        { id: 60023 },
+        { id: 60024 },
+        { id: 60095 }
       ]
     },
     {
-      name: 'I-26 (117 - 109)',
+      name: '26 Inner',
       camera: [
-        { id: 10019 },
-        { id: 10018 },
-        { id: 10039 },
-        { id: 10038 },
-        { id: 10036 },
-        { id: 10035 },
-        { id: 10034 },
-        { id: 10033 },
-        { id: 10015 },
-        { id: 10030 }
+        { id: 60031 },
+        // { id: 60032 }, Not working as of 2016-12-19
+        { id: 60033 },
+        { id: 60034 },
+        { id: 60036 },
+        { id: 60037 },
+        { id: 60038 },
+        { id: 60058 },
+        { id: 60060 },
+        { id: 60061 },
+        { id: 60062 },
+        { id: 60063 }
       ]
     },
     {
-      name: 'I-77',
+      name: 'Bridge',
       camera: [
-        { id: 10020 },
-        { id: 10122 },
-        { id: 10121 },
-        { id: 10120 },
-        { id: 10119 },
-        { id: 10118 },
-        { id: 10117 },
-        { id: 10116 },
-        { id: 10115 },
-        { id: 10114 }
-      ]
-    },
-    /*
-    {
-      name: 'West Ashley',
-      camera: [
-        { index: 307 },
-        { index: 308 },
-        { index: 309 },
-        { index: 313 },
-        { index: 319 },
-        { index: 310 },
-        { index: 311 },
-        { index: 312 },
-        // { index: 314 },
-        { index: 293 },
-        { index: 294 },
-        { index: 295 },
-        { index: 296 },
-        { index: 297 },
-        { index: 298 },
-        { index: 315 },
-        { index: 316 }
-      ]
-    },
-    {
-      name: 'Ravenel Bridge',
-      camera: [
-        { index: 326 },
-        { index: 327 },
-        { index: 328 },
-        { index: 329 },
-        { index: 330 },
-        { index: 331 },
-        { index: 332 },
-        { index: 333 },
-        { index: 334 },
+        { id: 60064 },
+        { id: 60065 },
+        { id: 60066 },
+        { id: 60067 },
+        { id: 60068 },
+        { id: 60069 },
+        { id: 60070 },
+        { id: 60071 }
       ]
     }
-    */
   ];
 
   $(function() {
@@ -178,17 +110,19 @@
       return parseInt(i) % n == r ? opts.fn(this) : opts.inverse(this);
     });
 
-    $.getJSON('assets/data/cameras-2016-05-05.json', function(data) {
+    $.getJSON('assets/data/cameras-2016-12-18.json', function(data) {
+
+      data.features.forEach(function (feature) {
+        if (feature.properties.region.indexOf('Charleston') > -1) {
+          if (feature.properties.title.indexOf('I-26') > -1) {
+          // console.log(feature.id + ': ' + feature.properties.region)
+            console.log(feature.id + ': ' + feature.properties.title);
+          }
+        }
+      });
 
       // Store all the cameras
       camera = data;
-
-      // Reverse route in afternoon
-      // if ((new Date()).getHours() > 12) {
-      //   for (var i = 0, j = route.length; i < j; i++) {
-      //     route[i].camera.reverse();
-      //   }
-      // }
 
       // Build HTML
       $('.compiled').html(Handlebars.compile($('#template').html())({
@@ -196,8 +130,23 @@
         camera: camera
       }));
 
+      $(document).on('click', '.toggle-camera-size', function (e) {
+        e.preventDefault();
+        setCameraSize();
+        Cookies.set(
+          'camera-size-small',
+          $('.toggle-camera-size .fa').hasClass('fa-th-large'),
+          { expires: 365 }
+        );
+      });
+
+      // Read Camera Size Cookies
+      if (Cookies.get('camera-size-small') === 'true') {
+        setCameraSize();
+      }
+
       // Load requested camera
-      $('body').on('click', 'a[href^="#"]', function(e) {
+      $('body').on('click', 'a[href^="#"][class!="toggle-camera-size"]', function(e) {
 
         // This stops the link from trying to go there in the DOM, but allows the URL to be updated
         e.stopPropagation();
@@ -211,22 +160,26 @@
 
     });
 
+    function setCameraSize() {
+      console.log('Setting Camera Size');
+      $('.col-camera').toggleClass('col-sm-4 col-sm-2');
+      $('.toggle-camera-size .fa').toggleClass('fa-th fa-th-large');      
+    }
+
     function updateView(hash) {
 
       // View enums + default view
       var ROUTE = '#route',
           CAMERA = '#camera',
           ADD = '#add',
-          view = ROUTE,
-          index = 0,
-          indexSet = false;
+          view = '',
+          id = '';
 
       if (hash !== '') {
         var parts = hash.split('-');
         view = parts[0];
         if (parts.length == 2) {
-          index = parseInt(parts[1]);
-          indexSet = true;
+          id = parts[1];
         }
       }
 
@@ -235,19 +188,18 @@
 
       // Switch tabs and show the correct view
       switch (view) {
-        // case ROUTE:
-        default:
-          showRoute(index);
-          showTab(ROUTE + '-' + index);
-        break;
-        case ADD:
-          showTab(ADD);
+        case ROUTE:
+          showRoute(id);
+          showTab(ROUTE + '-' + id);
         break;
         case CAMERA:
-          if (indexSet) {
-            showCamera(index);
+          if (id !== '') {
+            showCamera(id);
           }
           showTab(CAMERA);
+        break;
+        default:
+          showTab(ADD);
         break;
       }
     }
@@ -256,24 +208,23 @@
       $('a[href="' + hash + '"]').tab('show');
     }
 
-    function showCamera(index) {
-      setupPlayer('player', camera.features[index].properties);
+    function showCamera(id) {
+      setupPlayer('player', getCameraById(id).properties);
       $('#camera li').removeClass('active');
-      $('a[href="#camera-' + index + '"]').closest('li').addClass('active');
+      $('a[href="#camera-' + id + '"]').closest('li').addClass('active');
     }
 
-    function getIndexById(id) {
+    function getCameraById(id) {
       for (var i = 0, j = camera.features.length; i < j; i++) {
         if (camera.features[i].id === id.toString()) {
-          return i;
+          return camera.features[i];
         }
       }
     }
 
     function showRoute(index) {
       for (var i = 0, j = route[index].camera.length; i < j; i++) {
-        var k = route[index].camera[i].index ? route[index].camera[i].index : getIndexById(route[index].camera[i].id);
-        setupPlayer('route-' + index + '-camera-' + i, camera.features[k].properties);
+        setupPlayer('route-' + index + '-camera-' + i, getCameraById(route[index].camera[i].id).properties);
       }
     }
 
@@ -284,6 +235,7 @@
         stretching: 'fill',
         width: '100%',
         aspectratio: '4:3',
+        controls: false,
         playlist: [{
           sources: [
             { file: properties.rtmp_url },
